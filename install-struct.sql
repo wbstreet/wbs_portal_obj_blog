5ddaf20739c3360dbb651ed380eeed1fc434e6be
Konstantin Polyakov <kostyan_93@mail.ru>
2018-02-05 02:50:14 +0300

    image_id -> image_storage_id

--- install-struct.sql
+++ install-struct.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS `{TABLE_PREFIX}mod_wbs_portal_obj_blog_publication`;
 CREATE TABLE `{TABLE_PREFIX}mod_wbs_portal_obj_blog_publication` (
   `obj_id` int(11) NOT NULL,
-  `image_id` int(11),
+  `image_storage_id` int(11),
   `title` varchar(255) NOT NULL,
   `description` text NOT NULL,
   `text` text NOT NULL,
